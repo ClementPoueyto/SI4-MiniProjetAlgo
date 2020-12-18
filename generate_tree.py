f9baa8418719b37e077691e93b44198e173ef7e9
bertolottoloic <loic.bertolotto@etu.unice.fr>
2020-12-18 19:33:28 +0100

    change algo + generate tree

--- generate_tree.py
+++ generate_tree.py
@@ -4,7 +4,7 @@ import random
 #n : nombre de sommets
 #p : probabilité pour sommet etre rouge
 #q : probabilité pour arc etre bleu
-def generate_tree(n, p, q):
+def generate_tree_old(n, p, q):
     nE = n*(n - 1)
     eB = ['B' for _ in range(int(nE * q))]
     eR = ['R' for _ in range(nE - int(nE * q))]
@@ -23,4 +23,15 @@ def generate_tree(n, p, q):
             edges[k,j] = e[i]
             edges[j,k] = e[i+1]
             i+=2
+    return vertices, edges
+
+def generate_tree(n, p, q):
+    nE = n*(n - 1)
+    vertices = {}
+    edges = {}
+    for i in range(n):
+        vertices[i] = "R" if random.randint(0,100) < p*100 else "B"
+        for j in range(i):
+            edges[i,j] = "B" if random.randint(0,100) <= q*100 else "R"
+            edges[j,i] = "B" if random.randint(0,100) <= q*100 else "R"
     return vertices, edges
